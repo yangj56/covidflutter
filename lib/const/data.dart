0e613786f5999269f1c24f8acf0bf6699449bcf2
Yang <yangjing.yj56@gmail.com>
2020-07-11 15:03:43 +0800

    updated country list

---
+++ lib/const/data.dart
@@ -0,0 +1,46 @@
+import 'package:covoid_19/model/country.dart';
+
+List<Country> countryList = [
+  Country(
+    id: "test-1",
+    asset: "assets/svg/ad.svg",
+    description: "Beautiful country 1",
+    name: "Belgium-1",
+  ),
+  Country(
+    id: "test-2",
+    asset: "assets/svg/ae.svg",
+    description: "Beautiful country 2",
+    name: "Belgium-2",
+  ),
+  Country(
+    id: "test-3",
+    asset: "assets/svg/af.svg",
+    description: "Beautiful country 3",
+    name: "Belgium-3",
+  ),
+  Country(
+    id: "test-4",
+    asset: "assets/svg/ag.svg",
+    description: "Beautiful country 4",
+    name: "Belgium-4",
+  ),
+  Country(
+    id: "test-5",
+    asset: "assets/svg/ai.svg",
+    description: "Beautiful country 5",
+    name: "Belgium-5",
+  ),
+  Country(
+    id: "test-6",
+    asset: "assets/svg/al.svg",
+    description: "Beautiful country 6",
+    name: "Belgium-6",
+  ),
+  Country(
+    id: "test-7",
+    asset: "assets/svg/am.svg",
+    description: "Beautiful country 7",
+    name: "Belgium-7",
+  ),
+];
